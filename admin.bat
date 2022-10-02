1d4342e618c7f34cdda1538efccdf22eea9b51e2
wyf <72241996+wyf9@users.noreply.github.com>
2022-10-02 15:00:45 +0800

    Add files via upload

---
+++ admin.bat
@@ -0,0 +1,17 @@
+@echo off
+title wyfadmin
+set wver=Beta 0.2.1
+if %1==launch goto go
+if %1==back goto goto
+:go
+echo Wyfadmin version %wver%
+echo [%date% %time%]Version:%wver%>>running.log
+echo Copyright wyf9. All rights reserved.
+echo Now time:%date% %time%
+echo [%date% %time%]Progran started>>running.log
+:goto
+echo [%date% %time%]Waiting command>>running.log
+set /p com=^>^>
+echo [%date% %time%]Command:%com%>>running.log
+io %com%
+exit
