33eb5fa3781cd56266f1766523481899d8e2f7da
wyf9 <wyf01239@163.com>
2022-10-30 15:59:43 +0800

    调整
    single-chinese

--- single-chinese/admin.bat
+++ single-chinese/admin.bat
@@ -1,18 +1,16 @@
 @echo off
-set wver=Beta 2.1.0
-set wvdate=2022.9.3
+set wver=Beta 0.3.1
+set wvdate=2022.10.29
 title wyfadmin
 if %1==launch goto go
-if %1==back goto goto
+if %1==wback goto command
 if %1==exit goto exit
 if %1==dellog goto dlog
-if %1==cls goto cls
 if %1==wver goto wyfver
-if %1==sysver goto systemver
-if %1==whelp goto wyfhelp
+if %1==whelp goto whelp
 echo [%date% %time%]输入非预设命令>>running.log
 %1 %2 %3 %4 %5 %6 %7 %8 %9
-goto back
+goto wback
 :go
 echo ==========>>running.log
 echo [%date% %time%]程序启动>>running.log
@@ -22,39 +20,32 @@ echo Wyfadmin 版本：%wver%.
 echo 版权所有 wyf9. 保留所有权.
 echo 系统时间；%date% %time%
 echo [%date% %time%]正常启动>>running.log
-:goto
+:command
 echo [%date% %time%]等待命令>>running.log
 set /p com=^>^>
 echo [%date% %time%]执行命令:"%com%">>running.log
 %0 %com%
 exit
-:back
+:wback
 echo [%date% %time%]无错误返回>>running.log
-%0 back
+%0 wback
 :exit
 echo [%date% %time%]程序退出>>running.log
 exit
 :dlog
 del running.log
 exit
-:cls
-cls
-goto back
 :wyfver
 echo wyfadmin 版本：%wver%
 echo 打包日期：%wvdate%
 echo 版权所有 wyf9. 保留所有权.
 echo 联系方式：
-echo 邮箱：wyf01299@163.com
-echo 网站：wyf9.bj.bcebos.com
+echo 邮箱: wyf01299@163.com
+echo 网站: wyf9.bj.bcebos.com
 echo [%date% %time%]显示版本信息>>running.log
-goto back
-:systemver
-ver
-start %windir%\system32\winver.exe
-goto back
-:wyfhelp
+goto wback
+:whelp
 echo None
 echo [%date% %time%]显示内置帮助>>running.log
-goto back
+goto wback
 
