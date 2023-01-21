65ee8fcdd09cbc6cf8317a52b3c0cdc04bfcfd57
wyf9 <wyf01239@163.com>
2023-01-21 23:35:05 +0800

    new zh-cn 语言文件完成

--- io.bat
+++ io.bat
@@ -42,14 +42,12 @@ echo 语言: Chinese
 echo 打包日期：%wvdate%
 echo 版权所有 wyf9. 保留所有权.
 echo 项目主页: https://github.com/wyf01239/wyfadmin
-echo 联系方式：
-echo 邮箱: wyf01299@163.com
 echo [%date% %time%]显示版本信息>>running.log
 goto wback
 :whelp
 echo 带*的命令可输入"whelpmore 命令"查看详细信息
 echo 内部命令:
-echo exit          退出程序
+echo exit	退出程序
 echo wdellog       删除日志文件(running.log)并退出
 echo wver          显示版本信息
 echo whelp         显示此帮助信息
