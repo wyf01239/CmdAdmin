58c30a1e5bf347226cb67f9fd1082629f2b8e608
wyf9 <wyf01239@163.com>
2023-04-01 21:30:22 +0800

    增加退出功能

--- CmdAdmin.bat
+++ CmdAdmin.bat
@@ -1,5 +1,5 @@
 @echo off
-:: echo [%date% %time%]  >>%wpath%data\running.log
+:: echo [Info %date% %time%]  >>%wpath%data\running.log
 set cd=
 set date=
 set time=
@@ -7,7 +7,7 @@ set wlastpath=%cd%
 set wpath=%~dp0
 cd %wpath%
 set wver=0.6
-set wvdate=2023.3.26
+set wvdate=2023.4.1
 
 echo ================================ >>%wpath%data\running.log
 echo  ____                    __  ______      __                              >>%wpath%data\running.log
@@ -19,70 +19,62 @@ echo    \ \____/\ \_\ \_\ \_\ \___,_\ \_\ \_\ \___,_\ \_\ \_\ \_\ \_\ \_\ \_\ >>
 echo     \/___/  \/_/\/_/\/_/\/__,_ /\/_/\/_/\/__,_ /\/_/\/_/\/_/\/_/\/_/\/_/ >>%wpath%data\running.log
 echo. >>%wpath%data\running.log
 
-echo [%date% %time%] CmdAdmin v%wver% Build date %wvdate% >>%wpath%data\running.log
-echo [%date% %time%] Loading... >>%wpath%data\running.log
+echo [Info %date% %time%] CmdAdmin v%wver% Build date %wvdate% >>%wpath%data\running.log
+echo [Info %date% %time%] Loading... >>%wpath%data\running.log
 
 call sources\wAPIGetTimeToSec.bat
 set wLoadStart=%wAPIBack_GTTS%
-echo [%date% %time%] Load start timestamp in day: %wAPIBack_GTTS% >>%wpath%data\running.log
+echo [Info %date% %time%] Load start timestamp in day: %wAPIBack_GTTS% >>%wpath%data\running.log
 
 set wvyear=2023
 set wLoadModules=0
 
 if not exist %wpath%data\config\LangNow.wcfg (
     echo en-us>%wpath%data\config\LangNow.wcfg
-    echo [%date% %time%] [Warn] Lang file lost. Change to en-us. >>%wpath%data\running.log
+    echo [Warn %date% %time%] Lang file lost. Change to en-us. >>%wpath%data\running.log
     echo [CA] WARNING: Langusge File lost.
     echo [CA] Changed language to "en-us".
 )
 
 set /p wlangnow=<%wpath%data\config\LangNow.wcfg
-echo [%date% %time%] Now language: %wlangnow% >>%wpath%data\running.log
-set isadmin=False
+echo [Info %date% %time%] Now language: %wlangnow% >>%wpath%data\running.log
 
-whoami /groups | find "S-1-16-12288">nul && set isadmin=True
-
-echo [%date% %time%] Loading language file... >>%wpath%data\running.log
+echo [Info %date% %time%] Loading language file... >>%wpath%data\running.log
 for /f "eol=# delims=;" %%l in (%wpath%sources\langs\%wlangnow%.wlng) do (
     set %%l>nul
 )
-echo [%date% %time%] %lang___load_lang_ok% >>%wpath%data\running.log
+echo [Info %date% %time%] %lang___load_lang_ok% >>%wpath%data\running.log
 
-if %isadmin% == true (
-    echo [%date% %time%] %lang___have_admin% >>%wpath%data\running.log
-    title %lang__cmd% - CmdAdmin - Admin
-    echo [CA] CmdAdmin v%wver% %wvdate% - %wlangnow% - Admin. %lang__all_rights_reserved%
-) else (
-    title %lang__cmd% - CmdAdmin
-    echo [CA] CmdAdmin v%wver% %wvdate% - %wlangnow%. %lang__all_rights_reserved%
-)
+title %lang__cmd% - CmdAdmin
+echo [CA] CmdAdmin v%wver% %wvdate% - %wlangnow%.%lang__all_rights_reserved%
 
-echo [%date% %time%] %lang___init_modules% >>%wpath%data\running.log
+
+echo [Info %date% %time%] %lang___init_modules% >>%wpath%data\running.log
 for /f "eol=; delims=" %%i in ('dir %wpath%modules\ /b /s') do (
     call "%%i" /winit
     set /a wLoadModules=wLoadModules+1
     )
-echo [%date% %time%] %lang___init_modules_ok% >>%wpath%data\running.log
+echo [Info %date% %time%] %lang___init_modules_ok% >>%wpath%data\running.log
 
 doskey /OVERSTRIKE ca=%wpath%wCmd.bat $1 $2 $4 $5 $6 $7 $8 $9
 
 prompt $P$S$G$G$S
 
 call sources\wAPIGetTimeToSec.bat
-echo [%date% %time%] %lang___load_end_time%: %wAPIBack_GTTS% >>%wpath%data\running.log
+echo [Info %date% %time%] %lang___load_end_time%: %wAPIBack_GTTS% >>%wpath%data\running.log
 set /a wLoadSec=wAPIBack_GTTS-wLoadStart
-echo [%date% %time%] %lang___load_time%: %wLoadSec%s >>%wpath%data\running.log
+echo [Info %date% %time%] %lang___load_time%: %wLoadSec%s >>%wpath%data\running.log
 if %wLoadSec% LSS 1 (
     set wLoadSec=%lang__smaller% 1
-    echo [%date% %time%] [Info] %lang___load_time_small% >>%wpath%data\running.log
+    echo [Info %date% %time%] %lang___load_time_small% >>%wpath%data\running.log
 ) else if %wLoadSec% GTR 960 (
     if %time% == 0:0:* (
         set wLoadSec=[Error]
-        echo [%date% %time%] [Error] %lang___load_time_long% >>%wpath%data\running.log
+        echo [Error %date% %time%] %lang___load_time_long% >>%wpath%data\running.log
     )
 )
 
-echo [%date% %time%] %lang___load_modules_ok_1% %wLoadModules% %lang___load_modules_ok_2%. >>%wpath%data\running.log
+echo [Info %date% %time%] %lang___load_modules_ok_1% %wLoadModules% %lang___load_modules_ok_2%. >>%wpath%data\running.log
 if %wLoadModules% == 1 (
     echo [CA] %lang__loaded% %wLoadModules% %lang__module%.
 ) else (
@@ -90,6 +82,6 @@ if %wLoadModules% == 1 (
 )
 cd %wpath%wPath 2>nul
 cd %wlastpath% 2>nul
-echo [%date% %time%] CmdAdmin %lang___load_ok% >>%wpath%data\running.log
+echo [Info %date% %time%] CmdAdmin %lang___load_ok% >>%wpath%data\running.log
 echo -------------------------------- >>%wpath%data\running.log
 echo [CA] CmdAdmin %lang__load_ok%. (%wLoadSec%s)
--- wCmd.bat
+++ wCmd.bat
@@ -1,29 +1,45 @@
 @echo off
+rem if "%1" == "" goto 
 
-set isadmin=False
-whoami /groups | find "S-1-16-12288">nul && set isadmin=True
+echo [Info %date% %time%] %lang___running_command%: CA %1 %2 %3 %4 %5 %6 %7 %8 %9 >>%wpath%data\running.log
+title %lang__cmd% - CmdAdmin ...
 
-if %isadmin% == true (
-    title %lang__cmd% - CmdAdmin - Admin ...
-) else (
-    title %lang__cmd% - CmdAdmin ...
-)
-
-
-if "%1" == "/t" echo Test
+if "%1" == "/t" (
+    echo [Info %date% %time%] Test >>%wpath%data\running.log
+    echo Test
+    goto wend
+    )
 if "%1" == "/q" goto quit
+if "%1" == "/e" (
+    echo [Info %date% %time%]Quitting CMD... >>%wpath%data\running.log
+    exit 0
+)
 if "%1" == "/v" goto wver
+if "%1" == "/h" goto whelp
+echo [Warn %date% %time%] %lang___unknown_command% >>%wpath%data\running.log
+echo [CA] %lang__unknown_command%.
 goto wend
 
 :wver
-echo CmdAdmin v%wver%. %wvdate%
-echo %lang_wver_lang%: %wlangnow% - %lang_wver_nowlang%
-echo %lang_wver_copy% (c) %wvyear% wyf9. %lang__all_rights_reserved%
-echo %lang_wver_project_home%: https://github.com/wyf01239/CmdAdmin
+    echo [Info %date% %time%] Showing version >>%wpath%data\running.log
+    echo CmdAdmin v%wver%. %wvdate%
+    echo %lang_wver_lang%: %wlangnow% - %lang_wver_nowlang%
+    echo %lang_wver_copy% (c) %wvyear% wyf9. %lang__all_rights_reserved%
+    echo %lang_wver_project_home%: https://github.com/wyf01239/CmdAdmin
+    goto wend
+
+:quit
+    echo [Info %date% %time%] %lang___quitting% CmdAdmin... >>%wpath%data\running.log
+    echo [CA] %lang_quitting% CmdAdmin...
+    prompt $P$G
+    title %lang__cmd%
+    cd %wlastpath% 2>nul
+    start %wpath%sources\AltAddF10.vbs
+    cmd
+    exit 0
+
+:whelp
 
 :wend
-if %isadmin% == true (
-    title %lang__cmd% - CmdAdmin - Admin
-) else (
     title %lang__cmd% - CmdAdmin
-)
+    exit /b 0
